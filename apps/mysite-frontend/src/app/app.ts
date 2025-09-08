5e84c24f2d9d57a8d88d0d03406c91a27ed684b4
Michael Kopp <michaeljg.kopp@gmail.com>
2025-09-09 00:08:59 +0200

    add fortawesome icons

--- apps/mysite-frontend/src/app/app.ts
+++ apps/mysite-frontend/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary, FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
-  imports: [RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, FaIconComponent],
+  selector: 'mysite-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'mysite-frontend';
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit() {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';  // font awesome regular 1 of 3 bundles
+    this.faIconLibrary.addIcons(...fontAwesomeIcons); // add any icons you want to use in the app here
+  }
 }
