cdbffebebdc4d955ca5a2ec0c7a66c6e22f067e9
Michael Kopp <michaeljg.kopp@gmail.com>
2025-09-14 20:52:39 +0200

    remove unused logo for now

--- apps/mysite-frontend/src/app/layout/navbar.ts
+++ apps/mysite-frontend/src/app/layout/navbar.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { LogoComponent } from '../shared/logo/logo.component';
 @Component({
   selector: 'mysite-navbar',
-  imports: [RouterLink, FaIconComponent, LogoComponent],
+  imports: [RouterLink, FaIconComponent],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
