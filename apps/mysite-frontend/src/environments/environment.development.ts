d15fb7f036f54ef7c74685327c690bc1d7e220dd
Michael Kopp <michaeljg.kopp@gmail.com>
2025-09-30 20:47:12 +0200

    add FE environments

---
+++ apps/mysite-frontend/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  keycloak: {
+    url: "http://auth.localhost",
+    realm: "mkopp",
+    clientId: "mkopp"
+  },
+  API_URL: "http://api.localhost"
+};
---
+++ apps/mysite-frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  keycloak: {
+    url: "http://auth.mkopp.dev",
+    realm: "mkopp",
+    clientId: "mkopp"
+  },
+  API_URL: "http://api.mkopp.dev"
+};
