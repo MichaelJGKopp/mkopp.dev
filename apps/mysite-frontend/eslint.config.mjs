f81feb08e57810b05455fa5af8fe9f7470b9880c
Michael Kopp <michaeljg.kopp@gmail.com>
2025-10-06 21:12:55 +0200

    fix(eslint): change allowed component/directive prefixes to 'mysite'

--- apps/mysite-frontend/eslint.config.mjs
+++ apps/mysite-frontend/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: ['mysite'],
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: ['mysite'],
           style: 'kebab-case',
         },
       ],
