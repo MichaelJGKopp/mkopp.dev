1002d726a78761ee6864b511e823149f898140ae
Michael Kopp <michaeljg.kopp@gmail.com>
2025-11-16 16:54:21 +0100

    modify AbstractAuditingEntity for Hibernate timestamps, hashcode and equals

--- apps/mysite-backend/src/main/java/dev/mkopp/mysite/shared/persistence/jpa/AbstractAuditingEntity.java
+++ apps/mysite-backend/src/main/java/dev/mkopp/mysite/shared/persistence/jpa/AbstractAuditingEntity.java
@@ -1,31 +1,55 @@
 package dev.mkopp.mysite.shared.persistence.jpa;
 
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.io.Serializable;
-import java.time.Instant;
-
+/* 
+ * Base class for entities to provide auditing fields, hashcode and equals.
+ * T is the type of the entity's identifier.
+ * Identity, hashCode, and equals are based on the identifier only.
+ */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public abstract class AbstractAuditingEntity<T> implements Serializable {
 
+  private static final long serialVersionUID = 1L;
+
   public abstract T getId();
 
-  @CreatedDate
-  @Column(updatable = false, name = "created_date")
-  private Instant createdDate;
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
 
-  @LastModifiedDate
-  @Column(name = "last_modified_date")
-  private Instant lastModifiedDate;
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+
+    AbstractAuditingEntity<?> that = (AbstractAuditingEntity<?>) o;
+
+    return getId() != null && getId().equals(that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return getId() != null ? getId().hashCode() : System.identityHashCode(this);
+  }
 }
